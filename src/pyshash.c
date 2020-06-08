1705d6ac0caa5d5e81e4c3f4abbdecb06eed41c9
ronen25 <ronenk17@gmail.com>
2020-06-08 22:42:44 +0300

    Added shash files

---
+++ src/pyshash.c
@@ -0,0 +1,93 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include "simi.h"
+#include "simiw.h"
+
+static PyObject *method_charikar_hash64(PyObject *self, PyObject *args) {
+    const char *data;
+    Py_ssize_t data_len;
+
+    /* Extract the raw data and it's length */
+    if (!PyArg_ParseTuple(args, "s#", &data, &data_len))
+        return NULL;
+
+    /* Calculate the Charikar hash */
+    uint64_t hash = charikar_hash64(data, data_len);
+    
+    return PyLong_FromUnsignedLongLong(hash);
+}
+
+static PyObject *method_charikar_hash64_wide(PyObject *self, PyObject *args) {
+    const char *data;
+    Py_ssize_t data_len;
+    int stoken_size;
+
+    /* Extract the raw data and it's length */
+    if (!PyArg_ParseTuple(args, "s#i", &data, &data_len, &stoken_size))
+        return NULL;
+
+    /* Sanity-check the arguments */
+    if (stoken_size < 1) {
+        PyErr_SetString(PyExc_ValueError, "supertoken size must be >= 1");
+        return NULL;
+    }
+
+    /* Calculate the Charikar hash */
+    uint64_t hash = charikar_hash64_wide(data, data_len, stoken_size);
+    
+    return PyLong_FromUnsignedLongLong(hash);
+}
+
+static PyObject *method_chaikar_hamming_dist(PyObject *self, PyObject *args) {
+    uint64_t hash1, hash2;
+
+    /* Extract the two hashes */
+    if (!PyArg_ParseTuple(args, "kk", &hash1, &hash2))
+        return NULL;
+
+    return PyLong_FromLong((long)hamming_dist(hash1, hash2));
+}
+
+/* ======================================================================================== */
+
+/* Module method definition table */
+static struct PyMethodDef method_table[] = {
+    {   
+        "charikar_hash64", 
+        method_charikar_hash64, 
+        METH_VARARGS, 
+        "Calculates Charikar's hash for the given data" 
+    },
+    {   
+        "charikar_hash64_wide", 
+        method_charikar_hash64_wide, 
+        METH_VARARGS, 
+        "Calculates Charikar's hash for the given data, with variable supertoken size" 
+    },
+    {   
+        "hamming_dist", 
+        method_chaikar_hamming_dist, 
+        METH_VARARGS, 
+        "Calculates the Hamming distance between two 64-bit integers" 
+    },
+    { NULL, NULL, 0, NULL }
+};
+
+/* ======================================================================================== */
+
+/* Module definition and initialization */
+
+static struct PyModuleDef python_shash_moddef = {
+    PyModuleDef_HEAD_INIT,
+    "python-shash",
+    NULL,
+    -1,
+    method_table
+};
+
+PyMODINIT_FUNC PyInit_shash(void) {
+    return PyModule_Create(&python_shash_moddef);
+}
+
+/* ======================================================================================== */
